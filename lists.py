66e49f5a953ce44670935a604029029b729830eb
giteshtheone-code <giteshtheone@gmail.com>
2025-12-23 19:10:17 +0530

    Implementing lists
    Example of a list

---
+++ lists.py
@@ -0,0 +1,12 @@
+ist1=['python',2026,'Edition','ICSE']
+list2=[10,20,30,40,50]
+
+print(list1)
+print(list2)
+print(list1[0])
+print(list1[3],list1[2]) 
+print(list2[2])
+print(list2[4])
+print(type(list1))
+print(type(list2))
+print(list1+list2)
